34ecc659d4de860f13929d0bd6c6cf6d48b48ae7
momijiina <momijiina@example.com>
2026-03-31 10:36:12 +0900

    言語設定の初期値を修正し、ユーザーの言語を正しく取得するように変更

--- templates/main.php
+++ templates/main.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 use OCP\Util;
 
 // Load l10n for JavaScript
-$lang = 'en';
+$lang = '';
 try {
-	$factory = \OC::$server->getL10NFactory();
-	$lang = $factory->findLanguage('gantt');
+	$userLang = \OC::$server->getL10NFactory()->findLanguage();
+	if ($userLang) {
+		$lang = $userLang;
+	}
 } catch (\Exception $e) {}
 $l10nDir = __DIR__ . '/../js/l10n/';
-if (file_exists($l10nDir . $lang . '.js')) {
+if ($lang && $lang !== 'en' && file_exists($l10nDir . $lang . '.js')) {
 	Util::addScript('gantt', 'l10n/' . $lang);
 } else {
 	$baseLang = strstr($lang, '_', true);
-	if ($baseLang && file_exists($l10nDir . $baseLang . '.js')) {
+	if ($baseLang && $baseLang !== 'en' && file_exists($l10nDir . $baseLang . '.js')) {
 		Util::addScript('gantt', 'l10n/' . $baseLang);
 	}
 }
